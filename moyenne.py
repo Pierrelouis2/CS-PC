01ea6a76153d500eff6d46e3cd42466b1642d30a
Pierre-louis Telep <pierre-louis.telep@cpe.fr>
2022-04-26 08:58:30 +0200

    rez

---
+++ moyenne.py
@@ -0,0 +1,23 @@
+import sys
+from tkinter.tix import INTEGER
+tot = 0
+passe = True
+if len(sys.argv[1:]) ==0 :
+    print("aucune moyenne a calculer")
+
+else :
+    for i in sys.argv[1:] :
+        if i.isdigit() : 
+            if (int(i)<0) or (int(i) >20) :
+                print("note(s) non valide(s)")
+                passe = False
+                break
+            else:
+                tot +=  int(i)
+        else :
+            print("note(s) non valide(s) digit")
+            passe = False
+            break
+    if passe :
+        moy = tot/(len(sys.argv)-1)
+        print ("Moyenne = ",moy)
