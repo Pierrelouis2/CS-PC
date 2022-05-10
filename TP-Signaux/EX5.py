60a03d0a3137c86c09a6680df962acc911430d5f
Pierre-louis Telep <pierre-louis.telep@cpe.fr>
2022-05-10 11:42:13 +0200

    cs

---
+++ TP-Signaux/EX5.py
@@ -0,0 +1,18 @@
+import signal,time,sys,os
+import multiprocessing as mp
+
+def F(s, frame):
+    while True:
+        time.sleep(1)
+        print("boucle du fils")    
+
+Process = mp.Process(target= F, args =(None,None))
+
+Process.start()
+
+for i in range(5):
+    time.sleep(1)
+    print(f"tour n° {i}")
+    if i ==3:
+        print("Processus [%d] envoi le signal %d au processus %d" %(os.getpid(), signal.SIGKILL, Process.pid))
+        
