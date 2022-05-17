a2ddd8486d21fa9209c1b9c452eefa19f870c717
Pierre-louis Telep <pierre-louis.telep@cpe.fr>
2022-05-17 09:32:01 +0200

    ved

--- TP-Signaux/EX5.py
+++ TP-Signaux/EX5.py
@@ -19,6 +19,5 @@ Process.start()
 for i in range(5):
     time.sleep(1)
     print(f"tour n° {i}")
-    if i ==3:
-        print("Processus [%d] envoi le signal %d au processus %d" %(os.getpid(), signal.SIGKILL, Process.pid))
+
         
